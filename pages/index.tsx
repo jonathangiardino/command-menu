ded35540aa79801b9debd8a17b9ca55877e670cd
Jonathan Giardino <j.giardino@me.com>
2023-02-20 12:06:54 +0100

    Polish and fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,17 @@ import Head from "next/head";
 import clsx from "clsx";
 import { Inter } from "@next/font/google";
 
-import { rectangles } from "@/lib";
 import { CommandMenu } from "@/components/command-menu";
 import { Rectangle } from "@/components/shared";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const rectangles = [
+  "w-[189px] h-[256px] bg-white top-[85px] right-[220px]",
+  "w-[264px] h-[251px] bg-green-80 top-[105px] left-[140px]",
+  "w-[199px] h-[296px] bg-gray-80  bottom-[105px] right-[240px]",
+  "w-[199px] h-[260px] bg-purple-60  bottom-[55px] left-[280px]",
+];
 
 const Home: NextPage = () => {
   return (
